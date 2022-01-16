fa54882711598e0e7ec0c86585d6c3d8de7f9e72
max180643 <max180643@gmail.com>
2022-01-16 23:38:47 +0700

    Add ruTorrent script.

---
+++ ruTorrent/install_rutorrent.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+error() {
+  echo "\\e[91m$1\\e[39m"
+  exit 1
+}
+
+checkInternet() {
+  echo "Checking internet connection..."
+  wget -q --spider https://www.google.com/
+  if [ $? -eq 0 ]; then
+    echo "Online. Continuing..."
+  else
+    error "Offline. Please connect to internet then run the script again."
+  fi
+}
+
+checkInternet
+
+sudo docker pull linuxserver/rutorrent:latest || error "Failed to pull lastest ruTorrent docker image!"
+sudo docker run -d -p 8100:80 -p 5000:5000 -p 55639:55639 -p 6881:6881/udp --name rutorrent --restart=always -e USERID=1001 -e GROUPID=1001 -e TZ=Asia/Bangkok -v ~/rutorrent/config:/config -v ~/myTorrent/downloads:/downloads linuxserver/rutorrent:latest || error "Failed to run ruTorrent docker image!"
