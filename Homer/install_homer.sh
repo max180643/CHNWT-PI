51410f9e22364330e4bf9696617c68c6479707c5
max180643 <max180643@gmail.com>
2022-01-17 01:22:04 +0700

    Add Homer script.

---
+++ Homer/install_homer.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+error() {
+  echo "\\e[91m$1\\e[39m"
+  exit 1
+}
+
+checkInternet() {
+  echo "Checking internet connection..."
+  wget -q --spider https://www.google.com/
+  if [ $? -eq 0 ]; then
+    echo "Online. Continuing..."
+  else
+    error "Offline. Please connect to internet then run the script again."
+  fi
+}
+
+checkInternet
+
+sudo docker pull b4bz/homer:latest || error "Failed to pull lastest Homer docker image!"
+sudo docker run -d -p 80:8080 --name homer --restart=always -e UID=1001 -e GID=1001 ~/homer/assets:/www/assets b4bz/homer:latest || error "Failed to run Homer docker image!"
