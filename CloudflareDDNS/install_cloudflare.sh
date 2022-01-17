f9ee3af2777bb37fbf058595846ad2873058e73e
max180643 <max180643@gmail.com>
2022-01-17 23:03:06 +0700

    Add Cloudflare DDNS script.

---
+++ CloudflareDDNS/install_cloudflare.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+error() {
+  echo "\\e[91m$1\\e[39m"
+  exit 1
+}
+
+checkInternet() {
+  echo "Checking internet connection..."
+  wget -q --spider https://www.google.com/
+  if [ $? -eq 0 ]; then
+    echo "Online. Continuing..."
+  else
+    error "Offline. Please connect to internet then run the script again."
+  fi
+}
+
+checkInternet
+
+sudo docker pull oznu/cloudflare-ddns:latest || error "Failed to pull lastest Cloudflare DDNS docker image!"
+sudo docker run -d --name cloudflare --restart=always -e PUID=1001 -e PGID=1001 -e API_KEY=xxxxxxx -e ZONE=example.com -e SUBDOMAIN=subdomain -e PROXIED=true oznu/cloudflare-ddns:latest || error "Failed to run Cloudflare DDNS docker image!"
