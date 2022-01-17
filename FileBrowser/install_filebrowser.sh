96ac2ae1b7454375470748828ab9b5515f3a3d7f
max180643 <max180643@gmail.com>
2022-01-17 17:17:25 +0700

    Add FileBrowser script.

---
+++ FileBrowser/install_filebrowser.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+error() {
+  echo "\\e[91m$1\\e[39m"
+  exit 1
+}
+
+checkInternet() {
+  echo "Checking internet connection..."
+  wget -q --spider https://www.google.com/
+  if [ $? -eq 0 ]; then
+    echo "Online. Continuing..."
+  else
+    error "Offline. Please connect to internet then run the script again."
+  fi
+}
+
+checkInternet
+
+sudo docker pull filebrowser/filebrowser:latest || error "Failed to pull lastest FileBrowser docker image!"
+sudo docker run -d -p 8300:80 --name filebrowser --restart=always -e PUID=1001 -e PGID=1001 -v ~/drive:/srv -v ~/filebrowser/filebrowser.db:/database/filebrowser.db -v ~/filebrowser/settings.json:/config/settings.json filebrowser/filebrowser:latest || error "Failed to run FileBrowser docker image!"
