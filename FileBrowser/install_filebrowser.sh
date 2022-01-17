3ddd161fcf82c5d5a4573757a66bf12a55cae64b
max180643 <max180643@gmail.com>
2022-01-17 19:56:07 +0700

    Update FileBrowser script.

--- FileBrowser/install_filebrowser.sh
+++ FileBrowser/install_filebrowser.sh
@@ -17,5 +17,5 @@ checkInternet() {
 
 checkInternet
 
-sudo docker pull filebrowser/filebrowser:latest || error "Failed to pull lastest FileBrowser docker image!"
-sudo docker run -d -p 8300:80 --name filebrowser --restart=always -v ~/drive:/srv -v ~/filebrowser/filebrowser.db:/database.db -v ~/filebrowser/.filebrowser.json:/.filebrowser.json -u 1001:1001 filebrowser/filebrowser:latest || error "Failed to run FileBrowser docker image!"
+sudo docker pull filebrowser/filebrowser:s6 || error "Failed to pull lastest FileBrowser docker image!"
+sudo docker run -d -p 8300:80 --name filebrowser --restart=always -v ~/drive:/srv -v ~/filebrowser/filebrowser.db:/database/filebrowser.db -v ~/filebrowser/settings.json:/config/settings.json filebrowser/filebrowser:s6 || error "Failed to run FileBrowser docker image!"
