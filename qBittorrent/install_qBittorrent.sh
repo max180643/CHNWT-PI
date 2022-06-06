7775212428ddda45deb337018458eec1c0aecda0
max180643 <max180643@gmail.com>
2022-06-06 20:57:24 +0700

    Update qBittorrent script.

--- qBittorrent/install_qBittorrent.sh
+++ qBittorrent/install_qBittorrent.sh
@@ -18,4 +18,4 @@ checkInternet() {
 checkInternet
 
 sudo docker pull linuxserver/qbittorrent:arm64v8-latest || error "Failed to pull lastest qBittorrent docker image!"
-sudo docker run -d -p 8110:8080 -e WEBUI_PORT=8080 -p 6882:6881 -p 6882:6881/udp -v ~/qbittorrent/config:/config -v ~/myTorrent/downloads:/downloads -e PUID=1000 -e PGID=1000 -e TZ=Asia/Bangkok linuxserver/qbittorrent:arm64v8-latest || error "Failed to run qBittorrent docker image!"
+sudo docker run -d -p 8110:8080 -e WEBUI_PORT=8080 -p 6882:6881 -p 6882:6881/udp -v ~/qbittorrent/config:/config -v ~/myTorrent/downloads:/downloads --name qBittorrent --restart=always -e PUID=1000 -e PGID=1000 -e TZ=Asia/Bangkok linuxserver/qbittorrent:arm64v8-latest || error "Failed to run qBittorrent docker image!"
